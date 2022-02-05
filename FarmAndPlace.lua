09be6d4485576e15ab75d6855debadef2fbea0b1
Michael McKay <mamckay89@gmail.com>
2022-02-05 10:41:34 -0700

    Farm and place file

--- FarmAndPlace.lua
+++ FarmAndPlace.lua
@@ -1,17 +1,28 @@
 function detectWood()
     while turtle.detect() do
-        turtle.dig()
-        print("wood detected")
+        local success, data = turtle.inspect()
+        if success then
+            print(data.name)
+            if data.name == "minecraft:log" then
+                turtle.dig()
+                print("wood detected")
+            end
+        end
+    end
+    detectTreeSapling()
+end
+
+function detectTreeSapling()
+    while turtle.detect() do
+        local success, data = turtle.inspect()
+        if success then
+            if data.name != "minecraft:oak_sapling" then
+                turtle.place()
+                print("placing sapling")
+            end
+        end
     end
     pollForWood()
 end
 
-function pollForWood()
-    while not turtle.detect() do
-        print("No tree detected")
-        
-    end
-    detectWood()
-end
-
-pollForWood()
+detectTreeSapling()
