36b23befd2431b16dc360063a4193bd4dbd2b3a9
Michael McKay <mamckay89@gmail.com>
2022-02-05 10:49:06 -0700

    Farm and place file

--- FarmAndPlace.lua
+++ FarmAndPlace.lua
@@ -16,9 +16,15 @@ function detectTreeSapling()
     while turtle.detect() do
         local success, data = turtle.inspect()
         if success then
-            if data.name !== "minecraft:oak_sapling" then
-                turtle.place()
-                print("placing sapling")
+            if data.name == "minecraft:oak_sapling" then
+                print("sapling detected")
+            end
+            elseif data.name == "minecraft:oak_log" then
+                print("log detected")
+            end
+            else then
+                print(data.name)
+                print("Invalid block detected")
             end
         end
     end
